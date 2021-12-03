0f0f6683b73a779f3b1b147989cb975ccc83c95c
Aya Mahagna <aya.1996@hotmail.co.il>
2021-12-03 13:37:00 +0200

    Uodated

--- src/main/java/il/ac/haifa/cs/sweng/OCSFSimpleChat/ChatClientCLI.java
+++ src/main/java/il/ac/haifa/cs/sweng/OCSFSimpleChat/ChatClientCLI.java
@@ -9,71 +9,74 @@ import java.util.StringTokenizer;
 
 public class ChatClientCLI {
 
-	private SimpleChatClient client;
-	private boolean isRunning;
-	private static final String SHELL_STRING = "Enter message (or exit to quit)> ";
-	private Thread loopThread;
-	private ArrayList<String> buffer = new ArrayList<>();
+    private SimpleChatClient client;
+    private boolean isRunning;
+    private static final String SHELL_STRING = "Enter message (or exit to quit)> ";
+    private Thread loopThread;
+    private ArrayList<String> buffer = new ArrayList<>();
 
-	public ChatClientCLI(SimpleChatClient client) {
-		this.client = client;
-		this.isRunning = false;
-	}
+    public ChatClientCLI(SimpleChatClient client) {
+        this.client = client;
+        this.isRunning = false;
+    }
 
-	public void loop() throws IOException {
-		loopThread = new Thread(new Runnable() {
+    public void loop() throws IOException {
+        loopThread = new Thread(new Runnable() {
 
-			@Override
-			public void run() {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-				String message;
-				String message_2 = "Aya,Julian";
-				while (client.isConnected()) {
-					System.out.print(SHELL_STRING);
+            @Override
+            public void run() {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String message;
+                String message_2 = "Aya,Julian";
+                while (client.isConnected()) {
+                    System.out.print(SHELL_STRING);
 
-					try {
-						message = reader.readLine();
-						if (message.isBlank())
-							continue;
-						StringTokenizer buf = new StringTokenizer(message);
-						while (buf.hasMoreTokens()) {
-							buffer.add(buf.nextToken());
-						}
-						if (message.equalsIgnoreCase("#exit")) {
-							System.out.println("Closing connection.");
-								client.closeConnection();
-						} else {
-                            if (message.equalsIgnoreCase("#sendSubmitters"))
-							client.sendToServer(message_2);
-                            else if(buffer.get(0).equals("#send"))
-                                client.sendToServer(message.substring(5));
-						}
-					} catch (IOException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-					buffer = null;
-					buffer = new ArrayList<>();
-				}
-			}
-		});
+                    try {
+                        message = reader.readLine();
 
-		loopThread.start();
-		this.isRunning = true;
 
-	}
+                        if (message.isBlank())
+                            continue;
 
-	public void displayMessage(Object message) {
-		if (isRunning) {
-			System.out.print("(Interrupted)\n");
-		}
-		System.out.println("Received message from server: " + message.toString());
-		if (isRunning)
-			System.out.print(SHELL_STRING);
-	}
+                        StringTokenizer buf = new StringTokenizer(message);
+                        while (buf.hasMoreTokens()) {
+                            buffer.add(buf.nextToken());
+                        }
+                        if (buffer.get(0).equals("#sendSubmitters"))
+                            client.sendToServer(message_2);
+                        else if (buffer.get(0).equals("#send"))
+                            client.sendToServer(message.substring(5));
+                        else if (buffer.get(0).equals("#exit"))
+                            client.closeConnection();
 
-	public void closeConnection() {
-		System.out.println("Connection closed.");
-		System.exit(0);
-	}
+                    } catch (IOException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+
+                    buffer = null;
+                    buffer = new ArrayList<>();
+
+                }
+            }
+        });
+
+        loopThread.start();
+        this.isRunning = true;
+
+    }
+
+    public void displayMessage(Object message) {
+        if (isRunning) {
+            System.out.print("(Interrupted)\n");
+        }
+        System.out.println("Received message from server: " + message.toString());
+        if (isRunning)
+            System.out.print(SHELL_STRING);
+    }
+
+    public void closeConnection() {
+        System.out.println("Connection closed.");
+        System.exit(0);
+    }
 }
